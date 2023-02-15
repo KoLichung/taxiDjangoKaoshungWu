9fd266eef2c8149d06ddbe3fdc70ba1f0757afde
JasonKo <jason@kosbrother.com>
2023-02-15 15:19:33 +0800

    fix

--- app/lineBot/views.py
+++ app/lineBot/views.py
@@ -42,7 +42,7 @@ def callback(request):
                         preview_image_url='https://i.imgur.com/vxQMxtm.png'
                     )
                 else:
-                    message = TextSendMessage(text=f'{profile.displayName   } 這裡無法聯繫客服！\n在 APP 內正確設定，就會傳條件選股的變動，或到價通知給您喔！')
+                    message = TextSendMessage(text=f'{profile.display_name} 這裡無法聯繫客服！\n在 APP 內正確設定，就會傳條件選股的變動，或到價通知給您喔！')
 
                 line_bot_api.reply_message(event.reply_token, message)
         return HttpResponse()
