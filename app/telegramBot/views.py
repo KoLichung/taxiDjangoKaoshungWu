c8421fdbd11cac3cca84a94a6648b9399ae1b0b4
JasonKo <jason@kosbrother.com>
2023-02-16 09:37:34 +0800

    foix

--- app/telegramBot/views.py
+++ app/telegramBot/views.py
@@ -13,11 +13,14 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-@csrf_exempt
-def callback(request):
-    application = ApplicationBuilder().token('5889906798:AAFR2O_uTBq_ZGPaDkqyfsHkWKK7EQ6bxj0').build()
+# 要有 callback, 要先透過連結設定 webhook：
+# https://api.telegram.org/bot5889906798:AAFR2O_uTBq_ZGPaDkqyfsHkWKK7EQ6bxj0/setWebhook?url=https://chinghsien.com/telegram_bot/callback
+
+# @csrf_exempt
+# def callback(request):
+#     application = ApplicationBuilder().token('5889906798:AAFR2O_uTBq_ZGPaDkqyfsHkWKK7EQ6bxj0').build()
     
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+#     start_handler = CommandHandler('start', start)
+#     application.add_handler(start_handler)
     
-    application.run_polling()
+#     application.run_polling()
