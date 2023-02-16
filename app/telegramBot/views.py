ec3079ae650fda730b43cd50c438d0e600abe609
JasonKo <jason@kosbrother.com>
2023-02-16 10:51:48 +0800

    fix

--- app/telegramBot/views.py
+++ app/telegramBot/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 import requests
+import logging
 
 # 要有 callback, 要先透過連結設定 webhook：
 # https://api.telegram.org/bot5889906798:AAFR2O_uTBq_ZGPaDkqyfsHkWKK7EQ6bxj0/setWebhook?url=https://chinghsien.com/telegram_bot/callback
 
 TOKEN = '5889906798:AAFR2O_uTBq_ZGPaDkqyfsHkWKK7EQ6bxj0'
+logger = logging.getLogger(__file__)
 
 @csrf_exempt
 def callback(request):
@@ -25,10 +27,13 @@ def callback(request):
 
 def parse_message(message):
     print("message-->",message)
+    logger.info(f'message-->{message}')
     chat_id = message['message']['chat']['id']
     txt = message['message']['text']
     print("chat_id-->", chat_id)
     print("txt-->", txt)
+    logger.info(f'chat_id-->{chat_id}')
+    logger.info(f'txt-->{txt}')
     return chat_id,txt
  
 def tel_send_message(chat_id, text):
@@ -37,6 +42,6 @@ def tel_send_message(chat_id, text):
                 'chat_id': chat_id,
                 'text': text
                 }
-   
     r = requests.post(url,json=payload)
-    return r
+    logger.info(r)
+    # return r
