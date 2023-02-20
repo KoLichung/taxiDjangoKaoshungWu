6aab4e2c7df3ab6b2e007f2a4c5e2c3499509196
JasonKo <jason@kosbrother.com>
2023-02-20 19:05:51 +0800

    fix

--- app/telegramBot/views.py
+++ app/telegramBot/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from rest_framework.exceptions import APIException
+from modelCore.models import Customer, User, Case, UserCaseShip, CarTeam
+from datetime import datetime
+
 import requests
 import logging
 import json
@@ -24,7 +28,61 @@ def callback(request):
             texts = txt.split('\n')
 
             if texts[0] == '派單':
-                tel_send_message(chat_id,'這是派單!')
+                on_address = texts[1].replace('上車','').replace(':','').replace('：','')
+                off_address = texts[2].replace('下車','').replace(':','').replace('：','')
+                
+                case = Case()
+                case.case_state = 'wait'
+
+                now = datetime.now()
+                now_string = now.strftime('%Y%m%d')
+                case.create_time = now
+                case.carTeam = CarTeam.objects.all().first()
+
+                car_team_number_string = case.carTeam.get_car_team_number_string
+                case.case_number = f'{case.carTeam.name} ❤️{now_string}.{car_team_number_string}❤️'
+                
+                path = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+                
+                try:
+                    onUrl = path+on_address+"&key="+"AIzaSyCrzmspoFyEFYlQyMqhEkt3x5kkY8U3C-Y"
+                    logger.info(onUrl)
+                    response = requests.get(onUrl)
+                    logger.info(response.text)
+
+                    resp_json_payload = response.json()
+                    case.on_lat = resp_json_payload['results'][0]['geometry']['location']['lat']
+                    case.on_lng = resp_json_payload['results'][0]['geometry']['location']['lng']
+                except Exception as e:
+                    print(f'on location error {e}')
+                    logger.error(f'on location error {e}')
+                    tel_send_message(chat_id,'無法辨識上車地點')
+                    raise APIException("error")
+
+                try:
+                    offUrl = path+off_address+"&key="+"AIzaSyCrzmspoFyEFYlQyMqhEkt3x5kkY8U3C-Y"
+                    logger.info(response.text)
+                    response = requests.get(offUrl)
+                    logger.info(response.text)
+
+                    resp_json_payload = response.json()
+                    case.off_lat = resp_json_payload['results'][0]['geometry']['location']['lat']
+                    case.off_lng = resp_json_payload['results'][0]['geometry']['location']['lng']
+                except Exception as e:
+                    print(f'off location error {e}')
+                    logger.error(f'off location error {e}')
+
+                try:
+                    case.time_memo = texts[3].replace('時間','').replace(':','').replace('：','')
+                    case.memo = texts[4].replace('備註','').replace(':','').replace('：','')
+                except Exception as e:
+                    print(f'case memo error {e}')
+                    logger.error(f'case memo error {e}')
+
+                case.save()
+
+                tel_send_message(chat_id,f'{case.case_number}\n派單成功，正在尋找駕駛\n上車：{case.on_address}\n下車：{case.off_address}\n時間：{case.time_memo}\n備註：{case.momo}')
+
             elif texts[0] == '預約單':
                 tel_send_message(chat_id,'這是預約單!')
             elif texts[0] == '取消':
